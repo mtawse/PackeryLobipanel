265393e82ec33181efffed4aa79feb095fcc7a26
Martin Tawse <martintawse@gmail.com>
2017-05-01 23:35:24 +0100

    Add some chart examples with nvd3

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,6 +44,73 @@ $(function () {
             return (className.match (/(^|\s)col-\S+/g) || []).join(' ');
         }).addClass(gridClass);
         $grid.packery();
+        initTestChart();
     });
 
+    initTestChart();
+
+
+    function initTestChart() {
+        var data = [
+            {
+                key: "Cumulative Return",
+                values: [
+                    {
+                        "label" : "A" ,
+                        "value" : -29.765957771107
+                    } ,
+                    {
+                        "label" : "B" ,
+                        "value" : 0
+                    } ,
+                    {
+                        "label" : "C" ,
+                        "value" : 32.807804682612
+                    } ,
+                    {
+                        "label" : "D" ,
+                        "value" : 196.45946739256
+                    } ,
+                    {
+                        "label" : "E" ,
+                        "value" : 0.19434030906893
+                    } ,
+                    {
+                        "label" : "F" ,
+                        "value" : -98.079782601442
+                    } ,
+                    {
+                        "label" : "G" ,
+                        "value" : -13.925743130903
+                    } ,
+                    {
+                        "label" : "H" ,
+                        "value" : -5.1387322875705
+                    }
+                ]
+            }
+        ]
+
+        nv.addGraph(function() {
+            var chart = nv.models.multiBarHorizontalChart()
+                .x(function(d) { return d.label })
+                .y(function(d) { return d.value })
+                .margin({top: 30, right: 20, bottom: 50, left: 175})
+                .showValues(true)
+                //.tooltips(false)
+                .showControls(false);
+
+            chart.yAxis
+                .tickFormat(d3.format(',.2f'));
+
+            d3.select('#chart svg')
+                .datum(data)
+                .transition().duration(500)
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+        });
+    }
 });
